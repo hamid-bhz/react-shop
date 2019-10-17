9a9b07fecaf267a14ed34af0cbf364fff9585d7f
hamid-bhz <hamidbehzadi71@gmail.com>
2019-10-17 10:09:42 +0330

    complete sign in with google and store user data

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import HomePage from './Pages/homepage/homepage.component';
 import ShopPage from './Pages/shop/shop.component';
 import Header from './Components/header/header.component';
 import SignInUpPage from './Pages/sign-in-up/sign-in-up.component';
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfile } from './firebase/firebase.utils';
 import './App.css';
 
 class App extends React.Component{
@@ -19,9 +19,20 @@ class App extends React.Component{
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user })
-      console.log(user)
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfile(userAuth);
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          })
+        })
+      } else {
+        this.setState({ currentUser: userAuth });
+      }
     })
   }
 
