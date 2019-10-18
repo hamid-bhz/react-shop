30cb1d3315291f2d888f35524c0fcaf47ed194a4
hamid-bhz <hamidbehzadi71@gmail.com>
2019-10-18 19:00:12 +0330

    change data flow and route to collection

--- src/redux/directory/directory.reducer.js
+++ src/redux/directory/directory.reducer.js
@@ -1,10 +1,10 @@
 const initialState = {
     sections: [
-        { id: 1, title: 'HATS', linkUrl: '/hats', imageUrl: 'https://i.ibb.co/cvpntL1/hats.png' },
-        { id: 2, title: 'JACKETS', linkUrl: '/jackets', imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png' },
-        { id: 3, title: 'SNEAKERS', linkUrl: '/sneakers', imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png' },
-        { id: 4, title: 'WOMENS', linkUrl: '/womens', size: 'large', imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png' },
-        { id: 5, title: 'MENS', linkUrl: '/mens', size: 'large', imageUrl: 'https://i.ibb.co/R70vBrQ/men.png' }
+        { id: 1, title: 'HATS', linkUrl: 'shop/hats', imageUrl: 'https://i.ibb.co/cvpntL1/hats.png' },
+        { id: 2, title: 'JACKETS', linkUrl: 'shop/jackets', imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png' },
+        { id: 3, title: 'SNEAKERS', linkUrl: 'shop/sneakers', imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png' },
+        { id: 4, title: 'WOMENS', linkUrl: 'shop/womens', size: 'large', imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png' },
+        { id: 5, title: 'MENS', linkUrl: 'shop/mens', size: 'large', imageUrl: 'https://i.ibb.co/R70vBrQ/men.png' }
     ]
 }
 
